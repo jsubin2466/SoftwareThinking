5c6d2cdbe0c11ee8450b29846f51a5c96082954e
jsubin2466 <jsubin2004@gmail.com>
2026-04-05 17:32:14 +0900

    Update 2399.py

--- Lab09/2399.py
+++ Lab09/2399.py
@@ -1,9 +1,13 @@
 n=int(input())
 li=list(map(int, input().split()))
+li.sort  #큰거에서 작은거 뺴야하므로 sort 처리
 
 count = 0
+ttt = 0
+
 for i in range(n):
-    for j in range(n):
-        count += abs(li[i]-li[j])
-print(count)
+    count += li[i]*i-ttt #6 앞에 1,3 있으면 6-1+6-3=6*2-1-3인 것
+    ttt += li[i] ###ttt에는 작은 값들이 순서대로 담김 
+
+print(count*2)
 
